e40f2999120746a92216c745d0ed0ab18f6e8d0a
nickbrowne <nick.browne@theconversation.edu.au>
2015-11-10 16:37:28 +1100

    Copy KeysWithEntities class from TC

---
+++ spec/lib/i18n/hygiene/keys_with_entities_spec.rb
@@ -0,0 +1,28 @@
+require 'i18n'
+require 'i18n/hygiene'
+
+describe I18n::Hygiene::KeysWithEntities do
+  describe "#to_a" do
+    context "when one key that shouldn't contains an HTML entity" do
+      let(:locales) { [:en, :fr_fr] }
+      let(:keys_to_check) { %w[foo bar foo_html foo_markdown] }
+      let(:i18nwrapper) { instance_double(I18n::Hygiene::Wrapper, locales: locales, keys_to_check: keys_to_check) }
+      let(:collection) { I18n::Hygiene::KeysWithEntities.new(i18nwrapper: i18nwrapper) }
+
+      before do
+        allow(i18nwrapper).to receive(:value).with(:en, "foo") { "one two" }
+        allow(i18nwrapper).to receive(:value).with(:en, "bar") { "one&nbsp;two" }
+        allow(i18nwrapper).to receive(:value).with(:en, "foo_html") { "one&nbsp;two" }
+        allow(i18nwrapper).to receive(:value).with(:en, "foo_markdown") { "one&nbsp;two" }
+        allow(i18nwrapper).to receive(:value).with(:fr_fr, "foo") { "one&ngsp;two" }
+        allow(i18nwrapper).to receive(:value).with(:fr_fr, "foo_html") { "one&ngsp;two" }
+        allow(i18nwrapper).to receive(:value).with(:fr_fr, "foo_markdown") { "one&ngsp;two" }
+        allow(i18nwrapper).to receive(:value).with(:fr_fr, "bar") { "one two" }
+      end
+
+      it "returns the appropriate result" do
+        expect(collection.to_a).to eq ["en: bar", "fr_fr: foo"]
+      end
+    end
+  end
+end
