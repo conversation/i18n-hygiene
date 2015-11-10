22769d14b39c591ebd36215ea64f0a29056e2ead
nickbrowne <nick.browne@theconversation.edu.au>
2015-11-10 16:41:35 +1100

    Copy VariableChecker class from TC

---
+++ lib/i18n/hygiene/variable_checker.rb
@@ -0,0 +1,80 @@
+module I18n
+  module Hygiene
+    # Checks for mismatching interpolation variables. For example, if the value for an i18n key
+    # as defined in :en contains an interpolation variable, the value for that key as defined
+    # in any other locale must have a matching variable name.
+    class VariableChecker
+
+      NON_ENGLISH_LOCALES_TO_CHECK = [ :fr_fr ]
+
+      def initialize(key, i18n_wrapper)
+        @key = key
+        @i18n_wrapper = i18n_wrapper
+      end
+
+      def mismatched_variables_found?
+        NON_ENGLISH_LOCALES_TO_CHECK.each do |locale|
+          if key_defined?(locale)
+            return true unless variables_match?(locale)
+          end
+        end
+        false
+      end
+
+      def mismatch_details
+        if mismatched_variables_found?
+          details_array = []
+          NON_ENGLISH_LOCALES_TO_CHECK.each do |locale|
+            if key_defined?(locale)
+              details_array << mismatch_details_for_locale(locale) unless variables_match?(locale)
+            end
+          end
+          return details_array.join("\n")
+        else
+          return "#{@key}: no missing interpolation variables found."
+        end
+      end
+
+      private
+
+      def mismatch_details_for_locale(locale)
+        "#{@key} for locale #{locale} is missing interpolation variable(s): #{missing_variables(locale)}"
+      end
+
+      def missing_variables(locale)
+        variables(:en).reject { |v| variables(locale).include?(v) }.join(', ')
+      end
+
+      def key_defined?(locale)
+        @i18n_wrapper.key_found?(locale, @key)
+      end
+
+      def variables_match?(locale)
+        variables(locale) == variables(:en)
+      end
+
+      def variables(locale)
+        collect_variables(@i18n_wrapper.value(locale, @key))
+      end
+
+      def collect_variables(string)
+        return [] unless string.is_a?(String)
+        (rails_variables(string) + js_variables(string)).uniq.sort
+      end
+
+      def rails_variables(string)
+        string.scan(/%{\S+}/).map { |var_string| var_string.gsub(/[%{}]/, '').to_sym }
+      end
+
+      def js_variables(string)
+        without_markdown_italics(string.scan(/__\S+__/).map { |var_string| var_string.gsub("__", "").to_sym })
+      end
+
+      def without_markdown_italics(array)
+        @key.end_with?("_markdown") ? [] : array
+      end
+
+    end
+
+  end
+end
---
+++ spec/lib/i18n/hygiene/variable_checker_spec.rb
@@ -0,0 +1,85 @@
+require 'i18n'
+require 'i18n/hygiene'
+
+describe I18n::Hygiene::VariableChecker do
+  let(:i18n_wrapper) { instance_double("TC::I18nWrapper") }
+  let(:checker) { I18n::Hygiene::VariableChecker.new("test_key", i18n_wrapper) }
+  let(:checker_markdown) { I18n::Hygiene::VariableChecker.new("test_key_markdown", i18n_wrapper) }
+  let(:base_value) { "A sentence with a variable %{variable_key}"  }
+  let(:base_js_value) { "A sentence with a variable __variable_key__"  }
+  let(:matching_value) { "Translation with correct variable %{variable_key}"  }
+  let(:matching_js_value) { "Translation with correct variable __variable_key__"  }
+  let(:mismatch) { "Translation with wrong variable %{bad_key}" }
+  let(:mismatch_js) { "Translation with wrong variable __bad_key__" }
+  let(:mismatch_info) { "test_key for locale fr_fr is missing interpolation variable(s): variable_key"  }
+  let(:markdown_italic_en) { "Within Markdown __italics__ ignore" }
+  let(:markdown_italic_fr) { "Within Markdown __italiques__ ignore" }
+
+
+  describe '#mismatched_variables_found?' do
+    it 'returns true if a mismatched variable is found' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { base_value }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key") { mismatch }
+      expect(checker.mismatched_variables_found?).to be true
+    end
+
+    it 'returns true if a mismatched variable using JS syntax is found' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { base_js_value }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key") { mismatch_js }
+      expect(checker.mismatched_variables_found?).to be true
+    end
+
+    it 'returns false if no mismatched variable is found' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { base_value }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key") { matching_value }
+      expect(checker.mismatched_variables_found?).to be false
+    end
+
+    it 'returns false if no mismatched variable using JS syntax is found' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { base_js_value }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key") { matching_js_value }
+      expect(checker.mismatched_variables_found?).to be false
+    end
+
+    it 'returns false if markdown italics used in key with "_markdown" suffix' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key_markdown") { markdown_italic_en }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key_markdown") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key_markdown") { markdown_italic_fr }
+      expect(checker_markdown.mismatched_variables_found?).to be false
+    end
+
+    it 'returns true if markdown italics used in key with non "_markdown" suffix' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { markdown_italic_en }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key") { markdown_italic_fr }
+      expect(checker.mismatched_variables_found?).to be true
+    end
+
+    it 'returns false if key not defined in locale' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { base_value }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { false }
+      expect(checker.mismatched_variables_found?).to be false
+    end
+  end
+
+  describe '#mismatch_details' do
+    it 'returns expected report if a mismatched variable is found' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { base_value }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key") { mismatch }
+      expect(checker.mismatch_details).to eq(mismatch_info)
+    end
+
+    it 'returns "No mismatches found" if no mismatched variable is found' do
+      allow(i18n_wrapper).to receive(:value).with(:en, "test_key") { base_value }
+      allow(i18n_wrapper).to receive(:key_found?).with(:fr_fr, "test_key") { true }
+      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "test_key") { matching_value }
+      expect(checker.mismatch_details).to eq("test_key: no missing interpolation variables found.")
+    end
+  end
+
+end
