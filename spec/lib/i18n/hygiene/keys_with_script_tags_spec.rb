15812fe2f7c109b02e9c5310e636495fd581cc73
Eleanor KH <eleanorakh@gmail.com>
2017-06-15 11:20:25 +1000

    Rename locale checker to follow IEFT standard

--- spec/lib/i18n/hygiene/keys_with_script_tags_spec.rb
+++ spec/lib/i18n/hygiene/keys_with_script_tags_spec.rb
@@ -3,7 +3,7 @@ require 'i18n/hygiene'
 
 describe I18n::Hygiene::KeysWithScriptTags do
   describe "#to_a" do
-    let(:locales) { [:en, :fr_fr] }
+    let(:locales) { [:en, :fr] }
     let(:keys_to_check) { %w[foo bar] }
     let(:i18n_wrapper) { instance_double(I18n::Hygiene::Wrapper, locales: locales, keys_to_check: keys_to_check) }
     let(:collection) { I18n::Hygiene::KeysWithScriptTags.new(i18n_wrapper: i18n_wrapper) }
@@ -11,12 +11,12 @@ describe I18n::Hygiene::KeysWithScriptTags do
     before do
       allow(i18n_wrapper).to receive(:value).with(:en, "foo") { "one two" }
       allow(i18n_wrapper).to receive(:value).with(:en, "bar") { "<script>one two</script>" }
-      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "foo") { "<script>one two</script>" }
-      allow(i18n_wrapper).to receive(:value).with(:fr_fr, "bar") { "one two" }
+      allow(i18n_wrapper).to receive(:value).with(:fr, "foo") { "<script>one two</script>" }
+      allow(i18n_wrapper).to receive(:value).with(:fr, "bar") { "one two" }
     end
 
     it "returns keys that include script tags" do
-      expect(collection.to_a).to eq(["en: bar", "fr_fr: foo"])
+      expect(collection.to_a).to eq(["en: bar", "fr: foo"])
     end
   end
 end
