7d3789c6155a826ea56aafcfe43be9bce16a2a7f
nickbrowne <nickbrowne@users.noreply.github.com>
2017-07-06 17:07:10 +1000

    Expand Reporter

--- lib/i18n/hygiene/reporter.rb
+++ lib/i18n/hygiene/reporter.rb
@@ -1,7 +1,18 @@
 module I18n
   module Hygiene
     class Reporter
+      def concat(result)
+        @results ||= []
+        @results.push(result)
+      end
 
+      def results
+        @results
+      end
+
+      def passed?
+        @results.all? { |result| result.passed? }
+      end
     end
   end
 end
--- spec/lib/i18n/hygiene/reporter_spec.rb
+++ spec/lib/i18n/hygiene/reporter_spec.rb
@@ -1,5 +1,39 @@
 require 'i18n/hygiene/reporter'
+require 'i18n/hygiene/result'
 
 RSpec.describe I18n::Hygiene::Reporter do
+  let(:reporter) { I18n::Hygiene::Reporter.new }
+  let(:passed_result) { I18n::Hygiene::Result.new(:pass) }
+  let(:failed_result) { I18n::Hygiene::Result.new(:failure) }
 
+  describe "#concat" do
+    it "adds a result to the stack" do
+      2.times { reporter.concat passed_result }
+      expect(reporter.results.count).to eq 2
+    end
+  end
+
+  describe "#passed?" do
+    before do
+      reporter.concat(passed_result)
+      reporter.concat(passed_result)
+    end
+
+    context "all reports passed" do
+      it "returns true" do
+        expect(reporter.passed?).to eq(true)
+      end
+    end
+
+    context "one or more reports failed" do
+      before do
+        reporter.concat(failed_result)
+        reporter.concat(passed_result)
+      end
+
+      it "returns false" do
+        expect(reporter.passed?).to eq(false)
+      end
+    end
+  end
 end
