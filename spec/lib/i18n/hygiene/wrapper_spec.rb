982b7758eeb17e9627b1832e6183c83e34d32e4e
nickbrowne <nickbrowne@users.noreply.github.com>
2017-07-13 16:07:54 +1000

    Expand Wrapper specs to exercise default locales more

--- spec/lib/i18n/hygiene/wrapper_spec.rb
+++ spec/lib/i18n/hygiene/wrapper_spec.rb
@@ -5,7 +5,9 @@ describe I18n::Hygiene::Wrapper do
   let(:wrapper) { I18n::Hygiene::Wrapper.new(keys_to_skip: []) }
 
   before do
-    ::I18n.backend.send(:store_translations, :en, abuse_report: { button: { submit: "x" }})
+    [:en, :fr, :es].each do |locale|
+      ::I18n.backend.send(:store_translations, locale, abuse_report: { button: { submit: "x" }})
+    end
   end
 
   describe '#keys_to_check' do
@@ -15,22 +17,14 @@ describe I18n::Hygiene::Wrapper do
   end
 
   describe '#locales' do
-    it 'includes expected locales' do
-      [:en].each do |locale|
-        expect(wrapper.locales).to include(locale)
-      end
+    it 'includes all locales by default' do
+      expect(wrapper.locales).to eq [:en, :fr, :es]
     end
 
     context 'provided locales' do
       let(:wrapper) { I18n::Hygiene::Wrapper.new(locales: [:en, :fr]) }
 
-      before do
-        [:fr, :es].each do |locale|
-          ::I18n.backend.send(:store_translations, locale, abuse_report: { button: { submit: "x" }})
-        end
-      end
-
-      it "includes only locales filtered by provided" do
+      it "includes only locales provided" do
         expect(wrapper.locales).to eq [:en, :fr]
       end
     end
