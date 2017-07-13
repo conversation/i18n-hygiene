42e251784bcbd49b931d735203da30e9fd1c398d
nickbrowne <nickbrowne@users.noreply.github.com>
2017-07-13 15:56:44 +1000

    Pass configured wrapper into KeysWithEntities

--- lib/i18n/hygiene/checks/html_entity.rb
+++ lib/i18n/hygiene/checks/html_entity.rb
@@ -1,6 +1,7 @@
 require 'i18n/hygiene/checks/base'
 require 'i18n/hygiene/keys_with_entities'
 require 'i18n/hygiene/result'
+require 'i18n/hygiene/wrapper'
 
 module I18n
   module Hygiene
@@ -9,7 +10,9 @@ module I18n
         def run
           puts "Checking for phrases that contain entities but probably shouldn't..."
 
-          keys_with_entities = I18n::Hygiene::KeysWithEntities.new
+          wrapper = I18n::Hygiene::Wrapper.new(locales: config.locales)
+
+          keys_with_entities = I18n::Hygiene::KeysWithEntities.new(i18nwrapper: wrapper)
 
           keys_with_entities.each do |key|
             puts "- #{key}"
