2dacf881cd0a0a470493a915538360465c604b17
nickbrowne <nickbrowne@users.noreply.github.com>
2017-07-14 18:27:53 +1000

    Add check for unexpected script tags

---
+++ lib/i18n/hygiene/checks/script_tag.rb
@@ -0,0 +1,36 @@
+require 'i18n/hygiene/checks/base'
+require 'i18n/hygiene/keys_with_script_tags'
+require 'i18n/hygiene/result'
+require 'i18n/hygiene/wrapper'
+
+module I18n
+  module Hygiene
+    module Checks
+      class ScriptTag < Base
+        def run
+          puts "Checking for phrases that contain entities but probably shouldn't..."
+
+          wrapper = I18n::Hygiene::Wrapper.new(locales: all_locales)
+
+          keys_with_script_tags = I18n::Hygiene::KeysWithScriptTags.new(i18n_wrapper: wrapper)
+
+          keys_with_script_tags.each { |key| puts " - #{key}" }
+
+          puts "Finished checking.\n\n"
+
+          if keys_with_script_tags.any?
+            yield Result.new(:failure)
+          else
+            yield Result.new(:pass)
+          end
+        end
+
+        private
+
+        def all_locales
+          [config.primary_locale] + config.locales
+        end
+      end
+    end
+  end
+end
