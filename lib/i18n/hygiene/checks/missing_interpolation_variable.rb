940a2a7d0b737570853534ce9370ea2f409d6c0d
Keith Pitty <keith@keithpitty.com>
2017-07-17 21:03:30 +1000

    Remove unnecessary argument

--- lib/i18n/hygiene/checks/missing_interpolation_variable.rb
+++ lib/i18n/hygiene/checks/missing_interpolation_variable.rb
@@ -10,7 +10,7 @@ module I18n
         def run
           puts "Checking for mismatching interpolation variables..."
 
-          wrapper = I18n::Hygiene::Wrapper.new(keys_to_skip: config.keys_to_skip)
+          wrapper = I18n::Hygiene::Wrapper.new
 
           mismatched_variables = wrapper.keys_to_check(config.primary_locale).select do |key|
             checker = I18n::Hygiene::VariableChecker.new(key, wrapper, config.primary_locale, config.locales)
