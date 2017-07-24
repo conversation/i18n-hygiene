dc95c3ac2505be65c9a3e8606cdf95d0f44380dd
Keith Pitty <keith@keithpitty.com>
2017-07-24 14:25:12 +1000

    Simplify ScriptTag check
    Remove unnecessary intermediate class.

--- lib/i18n/hygiene.rb
+++ lib/i18n/hygiene.rb
@@ -1,7 +1,6 @@
 require 'i18n/hygiene/key_usage_checker'
 require 'i18n/hygiene/keys_with_matched_value'
 require 'i18n/hygiene/keys_with_return_symbol'
-require 'i18n/hygiene/keys_with_script_tags'
 require 'i18n/hygiene/locale_translations'
 require 'i18n/hygiene/variable_checker'
 require 'i18n/hygiene/wrapper'
